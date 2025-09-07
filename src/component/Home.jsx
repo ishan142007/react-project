29097d3bce4bcd3589118aaf476b5ed5f0e06a3c
ishan <ishanpatel9785@gmail.com>
2025-09-07 19:57:52 +0530

    made progress

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import './file.css';
+import {motion,useScroll} from 'framer-motion';
+import { animate } from "framer-motion/dom";
+
 // import Card from './Data';
 
+
 function Home({url,url2,url3}){
+    // let list=document.querySelectorAll(".ani");
+    let list=[
+        "https://educationsite-three.vercel.app/assets/8-DZ1QSHK9.png",
+        "https://educationsite-three.vercel.app/assets/9-CwlI4jBK.png",
+        "https://educationsite-three.vercel.app/assets/fleg1-BicyKCV0.png",
+        "https://educationsite-three.vercel.app/assets/flegs2-D3bNWwwd.png",
+        "https://educationsite-three.vercel.app/assets/3-BFs7xKsG.png",
+        "https://educationsite-three.vercel.app/assets/4-KXCRXRvQ.png",
+        "https://educationsite-three.vercel.app/assets/5-qdxULm4o.png",
+        "https://educationsite-three.vercel.app/assets/6-BHZth-GH.png",
+        "https://educationsite-three.vercel.app/assets/7-PlP78vLv.png"
+    ]
+    
+    console.log(list);
+   
+    const [position,setposition]=useState("0");
+   
+    const scrollYProgress=useScroll().scrollYProgress
     return(
         <>
         <div className=" w-full h-max flex justify-center items-center bg-green-100 ">
-            <div className="w-max h-max rounded-4xl shadow-2xl bg-white hover:scale-101 transition duration-300 p-15"> 
+            <div className="w-max h-max rounded-4xl shadow-2xl bg-white m-10 transition duration-300 "> 
 
             <section className="w-full h-30 flex">
                 <div className="w-[30%] h-max p-2"><img src={url} alt="logo" className="w-30 h-20 m-4 rounded-4xl"/></div>
@@ -19,7 +41,7 @@ function Home({url,url2,url3}){
                         <button className="bg-green-500 p-1 rounded">Free Consultation</button>
                         </div>
             </section>
-            <section className="text-center flex  mt-3 p-3 bg-green-100">
+            <section className="text-center flex  mt-3 p-3 bg-green-100 px-15">
                 <div className="w-[20%]  h-70 flex items-end">
                         <img src={url2} alt="" className=" h-20 m-5 animate-bounce" />
                 </div>
@@ -39,21 +61,115 @@ function Home({url,url2,url3}){
                     <img src={url3} alt="" className="h-19 animate-bounce"/>
                 </div>
             </section>
-            <section className="w-full mt-8 ">
+            <section className="w-full mt-8 px-15">
               <section>
-            <div className=" flex w-full h-50  rounded-2xl justify-between text-center ">
+            <div className=" flex w-full h-70  rounded-2xl justify-between text-center ">
 
-            <div className="w-50  m-5 rounded-2xl  hover:scale-101 transform hover:shadow-2xl shadow-emerald-950 duration-500">12000+ <br />students</div>
-            <div className="w-50  m-5 rounded-2xl hover:scale-101 transform hover:shadow-2xl shadow-emerald-950 duration-500">24+ <br />Years of experience</div>
-            <div className="w-50  m-5 rounded-2xl hover:scale-101 transform hover:shadow-2xl shadow-emerald-950 duration-500">15+ <br />countries</div>
+            <div className="w-50  pt-5 rounded-2xl  hover:scale-101 transform hover:shadow-2xl shadow-gray-400 duration-500">
+                <div className="m-8"><b className="text-3xl text-green-900">12000+</b><br/>Students  </div>
+            <div className="m-8"><b className="text-3xl text-green-900">4.8/5</b> <br />Ratings</div>
             </div>
-            <div className=" flex w-full h-50 rounded-2xl justify-between text-center ">
-
-           <div className="w-50 m-5 rounded-2xl hover:scale-101 transform hover:shadow-2xl shadow-emerald-950 duration-500">4.8/5 <br />Ratings</div>
-            <div className="w-50  m-5 rounded-2xl hover:scale-101 transform hover:shadow-2xl shadow-emerald-950 duration-500">95% <br />Visa Success Rate</div>
-            <div className="w-50  m-5 rounded-2xl hover:scale-101 transform hover:shadow-2xl shadow-emerald-950 duration-500">850+ <br />University Representation</div>
+            <div className="w-50  pt-5 rounded-2xl hover:scale-101 transform hover:shadow-2xl shadow-gray-400 duration-500">
+                <div className="m-8"><b className="text-3xl text-green-900">24+</b><br />Years of Experience</div>
+            <div className="m-8"><b className="text-3xl text-green-900">95%</b> <br />Visa Success Rate</div>
+            </div>
+            <div className="w-50  pt-5 rounded-2xl hover:scale-101 transform hover:shadow-2xl shadow-gray-400 duration-500">
+                <div className="m-8"><b className="text-3xl text-green-900">15+</b><br />Countries</div>
+            <div className="m-8"><b className="text-3xl text-green-900">850+</b> <br />University Representation</div>
+            </div>
             </div>
         </section>
+            <motion.div className="w-full h-3 fixed bg-green-700 top-0 left-0 origin-left"
+            style={{
+                scaleX:scrollYProgress
+            }}
+            
+            >
+
+            </motion.div>
+        
+            </section>
+            <section className="px-15">
+                <div className="flex-col justify-center ">
+                    <div className="text-center text-2xl">
+                        <h1>Global Study <b className="text-green-600">Destinations</b></h1>
+                        <h1 className="text-green-600 text-5xl mt-[-30px]">____</h1>
+                    </div>
+                <motion.div className="flex gap-0.5 mt-9 w-full h-40">
+                    {/* animation are to be given  */}
+                
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/8-DZ1QSHK9.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/9-CwlI4jBK.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/fleg1-BicyKCV0.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/flegs2-D3bNWwwd.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/3-BFs7xKsG.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/4-KXCRXRvQ.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/5-qdxULm4o.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/6-BHZth-GH.png" alt="" /></div>
+                    <div className="ani"><img src="https://educationsite-three.vercel.app/assets/7-PlP78vLv.png" alt="" /></div>
+                </motion.div>
+                </div>
+                  <div className="flex-col justify-center mt-10 w-full ">
+                    <div className="text-center text-2xl h-20">
+                        <h1> <b className="text-green-600">Universites </b>our Students got Admissions from</h1>
+                        <h1 className="text-green-600 text-5xl mt-[-30px]">____</h1>
+                    </div>
+                <motion.div className="flex gap-0.5 mt-9 h-20">
+                    {/* animation are to be given  */}
+                
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                    <div className="ani"><img src="" alt="" /></div>
+                </motion.div>
+                <div className="text-gray-400 text-center h-10 "><i>...and many more</i></div>
+                </div>
+            </section>
+            <section className="mt-20 px-15">
+                <div className="text-center text-2xl h-20">
+                        <h1> Admission <b className="text-green-600">Open</b></h1>
+                        <h1 className="text-green-600 text-5xl mt-[-30px]">____</h1>
+                    </div>
+                <div className="flex gap-20 justify-center w-full h-max mb-5 ">
+                   <div className="w-[30%] h-[90%]  rounded-xl  bg-green-100 hover:shadow-sm hover:shadow-green-300">
+                    <img src="https://educationsite-three.vercel.app/assets/Undergraduate%20Admissions-BRQwNMS8.png" alt="" />
+                    <p className="mx-5  py-2 ">
+                        <h1 className="font-bold text-xl">Undergraduate Admissions</h1>
+                        <span className="text-gray-500 font-semiboldbold" >Start strong with world-class Bachelor’s programs abroad.
+                            </span >
+                            </p>
+                            </div>
+
+
+                   <div className="w-[30%] h-[90%]  rounded-xl  bg-green-100 hover:shadow-sm hover:shadow-green-300">
+                    <img src="https://educationsite-three.vercel.app/assets/Postgraduate%20Admissions-CZXcxuix.png" alt="" />
+                    <p className="mx-5  py-2 ">
+                        <h1 className="font-bold text-xl">Postgraduate Admissions</h1>
+                        <span className="text-gray-500 font-semiboldbold" >Master your future with specialized global programs.
+                            </span >
+                    </p>
+                    </div>
+                   
+                   
+                   <div className="w-[30%] h-[90%]  rounded-xl  bg-green-100 hover:shadow-sm hover:shadow-green-300">
+                    <img src="https://educationsite-three.vercel.app/assets/MBA%20Admissions-FJOnvt5X.png" alt="" />
+                      <p className="mx-5  py-2 ">
+                      <h1 className="font-bold text-xl">MBA Admissions</h1>
+                        <span className="text-gray-500 font-semiboldbold" >Advance your career with top-ranked global business schools.
+                            </span >
+                            </p>
+                    </div>
+                </div>
+            </section>
+            <section className="bg-black">
+                <div className="w-full h-80 border ">
+                  
+                </div>
             </section>
             </div>
         </div>
